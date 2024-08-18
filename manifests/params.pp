75f89f9abbbbc99fe8034b6ee832af7f703fbe26
root <root@5a996f21371c.mylabserver.com>
2024-08-18 20:59:35 +0000

    Refactered install class for Debian family servers

---
+++ manifests/params.pp
@@ -0,0 +1,16 @@
+# @summary 
+#
+#  Operating Systems related variables (and others)
+#
+
+class apache::params {
+  $install_ensure = 'present'
+  case $facts['os']['family'] {
+    'RedHat': {
+      $install_name = 'httpd'
+    }
+    'Debian': {
+      $install_name = 'apache2'
+    }
+  }
+}
